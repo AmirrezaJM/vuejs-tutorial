a932cc3f157e1628f19442e21822361a486b2eeb
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-03-31 17:55:39 +0200

    lecture4-start

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// explain v-if in the project
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
