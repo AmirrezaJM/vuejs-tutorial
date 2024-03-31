f064a9ff0e078b2dcd05adbeff58591eaba21196
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-03-31 18:01:03 +0200

    lecture6-start

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// methods in vuejs and writing this to point to variables in data.
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -17,13 +17,6 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        // solution
         updateImage(variantImage) {
             this.image = variantImage
         }
