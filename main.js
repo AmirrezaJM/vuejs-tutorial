b7fdec0bb9728c4717f01784ecd3594ef95177db
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-03-31 17:58:04 +0200

    lecture4-end

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+// make an array object and use v-for to repeat the varients
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            // solution
+            sizes: ['S', 'M', 'L', 'XL'],
+            // solution
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
