719b8936b8da47820b340917515a0d613de11999
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-03-31 17:51:50 +0200

    lecture2-end

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+/// we can use v-bind to assign them a string value dynamically, much like with other attributes. However,
+/// trying to generate those values using string concatenation can be annoying and error-prone
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
         }
     }
 })
