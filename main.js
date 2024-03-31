b03d7f7979aa3a8f7ce60ea3398ad1b34d4c87c9
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-03-31 17:48:21 +0200

    lecture1-end

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+// create application with the function createApp 
+// In here we use option API Method of vue 
+// Option API will use in Vue 2 and Vue 3. but right now, it's not so popular as compostion API but in here I will teach both.
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.' 
+            // Solution
+        }
+    }
+})
